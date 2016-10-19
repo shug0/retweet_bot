3f9885bad7f53f96519db0fef92303eae20681e6
Thomas Alberola <thomas@needacoffee.fr>
2016-10-19 15:22:28 +0200

    Adding following security
    Now the bot is checking if the user who tweeted to the bot is in the list AND IF the bot is following him.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// Load NPM LIB
 var Twit = require('twit')
+// JS function for looking in array and return true if users are in it
+var contains = function(needle) {var findNaN = needle !== needle; var indexOf; if(!findNaN && typeof Array.prototype.indexOf === 'function') {indexOf = Array.prototype.indexOf; } else {indexOf = function(needle) {var i = -1, index = -1; for(i = 0; i < this.length; i++) {var item = this[i]; if((findNaN && item !== item) || item === needle) {index = i; break; } } return index; }; } return indexOf.call(this, needle) > -1; };
 
 // Twiter Credentials (KEEP IT PRIVATE !)
 var T = new Twit({
@@ -10,25 +13,31 @@ var T = new Twit({
 })
 
 // List of Twitter users who will be RT by the Bot
-var streamers = ['le_bot_de_shug0','Shug0', 'Alphacast_fr', 'meltyDegun', 'manaberryio', 'meltymoonL', 'nujaow', 'Xeleko', 'xsooesport'];
+var streamers = ['Alphacast_fr', 'meltyDegun', 'manaberryio', 'meltymoonL', 'nujaow', 'Xeleko', 'xsooesport'];
 
-// Mention for get RT  
+// Mention for get RT
 var watcher = '@OversquadFR';
 
-// JS function for looking in array and return true if users are in it
-var contains = function(needle) {var findNaN = needle !== needle; var indexOf; if(!findNaN && typeof Array.prototype.indexOf === 'function') {indexOf = Array.prototype.indexOf; } else {indexOf = function(needle) {var i = -1, index = -1; for(i = 0; i < this.length; i++) {var item = this[i]; if((findNaN && item !== item) || item === needle) {index = i; break; } } return index; }; } return indexOf.call(this, needle) > -1; };
+// User to check following
+var userToCheckFollow = 'OversquadFR';
+var followingsUsers = [];
 
-
+// Store the IDS of the followings accounts
+T.get('followers/ids', { screen_name: userToCheckFollow },  function (err, data, response) {
+  followingsUsers = data.ids;
+})
 
 var stream = T.stream('statuses/filter', { track: watcher });
+
 stream.on('tweet', function (tweet) {
 
-  // The tweet with the watcher is found
-  if (contains.call(streamers, tweet.user.screen_name)) {
+  // Test if the user who had tweeted is in the streamers list & if the 'userToCheckFollow' is following him
+  if ( contains.call(streamers, tweet.user.screen_name) && contains.call(followingsUsers, tweet.user.id)) {
 
-    // The user is in the list so the bot RT this tweet
+    // We RT his tweet
     T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
-      //console.log(data)
+      console.log('Just RT @' + tweet.user.screen_name);
     })
+
   }
 })
